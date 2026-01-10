9ef9f781768aeda4ae7a8a9d58c76573280b238d
Pritam Rakshit <135799297+RAKSHITPRITAM002@users.noreply.github.com>
2026-01-10 12:52:58 +0530

    great game

--- 03_Password_generator.py
+++ 03_Password_generator.py
@@ -2,6 +2,6 @@ import random
 char='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()-_+={}[]|\\;:"<>,./?'
 length=int(input("Enter password length:"))
 password = ''.join(random.choice(char) for i in range(length))
-print("Generate password", password)
+print("Generate password:", password)
 
     
---
+++ 05_rock_paper_scissor.py
@@ -0,0 +1,21 @@
+import random
+
+while True:
+    choice = input('Rock, Paper or Scissors (r/p/s) (q to quit): ').lower()
+    if choice == 'q':
+        print("Goodbye!")
+        break
+    if choice not in ('r', 'p', 's'):
+        print("Invalid Choice!")
+        continue
+
+    computer = random.choice(['r', 'p', 's'])
+    print(f'Computer chose: {computer}')
+
+    if choice == computer:
+        print("It's a tie!")
+    elif (choice == 'r' and computer == 's') or (choice == 's' and computer == 'p') or (choice == 'p' and computer == 'r'):
+        print("YOU WIN! HOORAY")
+    else:
+        print("YOU LOSE :(")
+    
