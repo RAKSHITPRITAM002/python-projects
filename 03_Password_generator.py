cd650b773ff12b6ac9406810dd0aa90b4327547c
Pritam Rakshit <135799297+RAKSHITPRITAM002@users.noreply.github.com>
2026-01-10 10:39:54 +0530

    updt 2.0

--- 03_Password_generator.py
+++ 03_Password_generator.py
@@ -3,4 +3,5 @@ char='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()-_+
 length=int(input("Enter password length:"))
 password = ''.join(random.choice(char) for i in range(length))
 print("Generate password", password)
+
     
