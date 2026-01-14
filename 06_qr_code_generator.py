8ecb03479446e0a741d7d8c698744e7ad01405e6
Pritam Rakshit <135799297+RAKSHITPRITAM002@users.noreply.github.com>
2026-01-14 10:32:22 +0530

    good to go

--- 06_qr_code_generator.py
+++ 06_qr_code_generator.py
@@ -1,8 +1,9 @@
 import qrcode
-data=input('Enter the Text or Url:  ').strip()
-filename=input('Enter The filename:   ').strip()
-qr=qrcode.QRCode(box_size=10, border=4)
+
+data = input('Enter the text or URL: ').strip()
+filename = input('Enter the filename: ').strip()
+qr = qrcode.QRCode(box_size=10, border=4)
 qr.add_data(data)
-image=qr.make_image(fill_colour="black",back_color="white")
+image = qr.make_image(fill_color='black', back_color='white')
 image.save(filename)
-print(f"qr code successfully add as {filename}")
+print(f'QR code saved as {filename}')
